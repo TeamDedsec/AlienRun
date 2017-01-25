41b523bf5781bbddb71d1f34617a97628dbfe252
KaloyanIT <kaloyanit@yahoo.com>
2017-01-25 23:14:43 +0200

    Change main activity Add constants class

--- app/src/main/java/com/example/kaloyanit/alienrun/MainActivity.java
+++ app/src/main/java/com/example/kaloyanit/alienrun/MainActivity.java
@@ -1,13 +1,25 @@
 package com.example.kaloyanit.alienrun;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.util.DisplayMetrics;
+import android.view.Window;
+import android.view.WindowManager;
+import com.example.kaloyanit.alienrun.Utils.BasicConstants;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+
+        DisplayMetrics dm = new DisplayMetrics();
+        getWindowManager().getDefaultDisplay().getMetrics(dm);
+        BasicConstants.SCREEN_WIDTH = dm.widthPixels;
+        BasicConstants.SCREEN_HEIGHT = dm.heightPixels;
+
         setContentView(R.layout.activity_main);
     }
 }
---
+++ app/src/main/java/com/example/kaloyanit/alienrun/Utils/BasicConstants.java
@@ -0,0 +1,14 @@
+package com.example.kaloyanit.alienrun.Utils;
+
+import android.content.Context;
+
+/**
+ * Created by KaloyanIT on 1/25/2017.
+ */
+
+public class BasicConstants {
+    public static int SCREEN_WIDTH;
+    public static int SCREEN_HEIGHT;
+
+    public static Context CURRENT_CONTEXT;
+}
