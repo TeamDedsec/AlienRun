adb28027249ea53838be800bd03f713d4a3164f5
Barrelrolla <chetkara@gmail.com>
2017-02-06 12:10:55 +0200

    Added enemy class

---
+++ app/src/main/java/com/example/kaloyanit/alienrun/Factories/EnemyFactory.java
@@ -0,0 +1,8 @@
+package com.example.kaloyanit.alienrun.Factories;
+
+/**
+ * Created by julian.teofilov on 3/2/2017.
+ */
+
+public class EnemyFactory {
+}
---
+++ app/src/main/java/com/example/kaloyanit/alienrun/GameObjects/Enemy.java
@@ -0,0 +1,8 @@
+package com.example.kaloyanit.alienrun.GameObjects;
+
+/**
+ * Created by julian.teofilov on 3/2/2017.
+ */
+
+public class Enemy {
+}
